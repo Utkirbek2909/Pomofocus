d66ef9200650e65545255ac2bb2ef971d11fc40d
utkirbek <utkirbe2909@gmail.com>
2025-03-08 07:09:10 +0500

    0 qiymatli malumot2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,26 +5,22 @@ import { useState } from 'react'
 
 export default function Home() {
   const [mode, setMode] = useState("Pomodoro")
-const defaultColors = ["rgb(186,73,73)", "rgb(56,133,138)", "rgb(57,112,151)"];
+  const defaultColors = ["rgb(186,73,73)", "rgb(56,133,138)", "rgb(57,112,151)"];
   const colorTheme = JSON.parse(localStorage.getItem('colorThemes')) || defaultColors;
-  
-  console.log(defaultColors[0]);
-  
   const bgColor = () => {
-    if(mode === "Pomodoro"){
+    if (mode === "Pomodoro") {
       return colorTheme[0] || defaultColors[0];
-    }else if(mode === "Short break"){
+    } else if (mode === "Short break") {
       return colorTheme[1] || defaultColors[1];
-    }else if(mode === "Long break"){
+    } else if (mode === "Long break") {
       return colorTheme[2] || defaultColors[2];
     }
   }
-
   return (
     <div className="w-full min-h-[100vh] text-white" style={{ backgroundColor: bgColor() }}>
       <Header mode={mode} setMode={setMode} />
       <main className='pb-[100px]'>
-        <Timer mode={mode} setMode={setMode} bgColor={bgColor}/>
+        <Timer mode={mode} setMode={setMode} bgColor={bgColor} />
         <Task />
       </main>
     </div>
